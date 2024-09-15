5726ae1a1f8e1ccff90eb98904ba6c00472b8ae7
zhritik007 <ghritiksingh21@gmail.com>
2024-09-15 11:42:02 +0530

    Improved CSS

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,7 @@ function Contact() {
       <p>Email: ghritiksingh21@gmail.com</p>
       <p>LinkedIn: <a href="https://linkedin.com/in/hritik-singh-024460197" target="_blank">linkedin.com/in/hritik-singh</a></p>
       <p>GitHub: <a href="https://github.com/zhritik007" target="_blank">github.com/zhritik007</a></p>
+      <p>Contact No. : +91 7838571622</p>
     </section>
   );
 }
