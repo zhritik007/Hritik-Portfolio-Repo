55b17d40f1a0d45f9cadd829157a36cb053e8602
zhritik007 <ghritiksingh21@gmail.com>
2024-09-18 09:01:15 +0530

    Random-Quote

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import "./Projects.css";
 import luckyRollsImg from '../../assets/lucky-rolls.png';
 import scoreKeeperImg from '../../assets/score-keeper.png';
 import spotifyCloneImg from '../../assets/spotify-clone.png';
+import randomQImg from '../../assets/random-quotes.png'
 
 function Projects() {
   return (
@@ -46,6 +47,20 @@ function Projects() {
             Live Demo
           </a>
         </div>
+
+        <div className="project-card">
+          <img src={randomQImg} alt="Random-Quote" />
+          <h3>Random Quote Generator</h3>
+          <p>A React-based app that fetches random quotes from the Ron Swanson Quotes API. Users can save their favorite quotes to a personal list, featuring dynamic API fetching, state management, and a responsive, user-friendly design.</p>
+          <a
+            href="https://zhritik007.github.io/quote-generator-repo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live Demo
+          </a>
+        </div>
+
       </div>
     </section>
   );
